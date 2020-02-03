ee1de50e936775ec2adab796d151bd0b36eadc16
Dave O'Brien <dave.o@mmtdigital.co.uk>
2020-02-03 21:26:47 +0000

    Add basic logo and content to entry page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/images/logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <img src={logo} className="logo" alt="Dave O'Brien, JavaScript & UI Developer" />
+      <div className="content">
+        <a href="/assets/daveobrien-cv-202001.pdf" download className='link'>Download my CV</a>
+        <p>A small update is coming soon. Serious. However, it is arguable whether coming back is worth your while, but if you're curious, try again next week (that's the 10th Feb 2020). It can't hurt.</p>
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,15 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('renders cv link', () => {
+  it('should render logo', () => {
+    const { getByAltText } = render(<App />);
+    const imageElement = getByAltText(/Dave O'Brien, JavaScript & UI Developer/i);
+    expect(imageElement).toBeInTheDocument();  
+  })
+  it('should render cv link', () => {
+    const { getByText } = render(<App />);
+    const linkElement = getByText(/Download my CV/i);
+    expect(linkElement).toBeInTheDocument();
+  })
 });
