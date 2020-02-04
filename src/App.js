becdb25644f17a794ec91a2e5cb0007ba10f34a5
Dave O'Brien <dave.o@mmtdigital.co.uk>
2020-02-04 17:38:43 +0000

    Update description on entry page

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
       <img src={logo} className="logo" alt="Dave O'Brien, JavaScript & UI Developer" />
       <div className="content">
         <a href="/assets/daveobrien-cv-202001.pdf" download className='link'>Download my CV</a>
-        <p>A small update is coming soon. Serious. However, it is arguable whether coming back is worth your while, but if you're curious, try again next week (that's the 10th Feb 2020). It can't hurt.</p>
+        <p>A small update is coming soon. Serious.</p>
+        <p>That said, it is arguable whether coming back is really worth your while but if you're curious, try again next week (that's the <b>10th Feb 2020</b>). If this date is in the past then shame on me. And if you think this is JavaScript Date object then <a href="https://github.com/daveobriencouk/2020.daveobrien.co.uk/blob/master/src/App.js">shame on you</a>.</p>
       </div>
     </div>
   );
